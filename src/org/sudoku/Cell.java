5d00b5b4dff798ce435b1ad7fdd88b233343674c
JulianFah <fah.julian@protonmail.com>
2020-04-14 16:43:37 +0200

    Added full gameplay functionality

--- src/org/sudoku/Cell.java
+++ src/org/sudoku/Cell.java
@@ -1,36 +1,84 @@
 package org.sudoku;
 
+import java.awt.Graphics;
 import java.awt.Color;
+import java.awt.Font;
 
 public class Cell {
     /**
-     * One Cell of the GUI's Sudoku bord.
+     * Part of the GUI's Sudoku board.
      */
+
     public static final int SIZE = 50;
 
     private int[] pos;
     private int value, provisionalValue;
+    private boolean selected;
     private Color color;
 
     public Cell(int[] pos, int value) {
-        this.pos = new int[2];
-        this.pos[0] = pos[0] * SIZE;
-        this.pos[1] = pos[1] * SIZE;
+        this.pos = pos;
+        this.value = value;
+        this.provisionalValue = 0;
+        this.color = Color.BLACK;
+    }
+    
+    public void render(Graphics g) {
+        if (selected) {
+            g.setColor(Color.RED);
+            g.fillRect(pos[0], pos[1], SIZE, SIZE);
+            g.setColor(Color.WHITE);
+            g.fillRect(pos[0] + 2, pos[1] + 2, SIZE - 4, SIZE - 4);
+        }
+
+        if (value != 0) {
+            g.setColor(Color.BLACK);
+            g.setFont(new Font("Arial", 1, 40));
+            g.drawString(String.valueOf(value), pos[0] + 10, pos[1] + 40);
+        } else if (provisionalValue != 0) {
+            g.setColor(Color.DARK_GRAY);
+            g.setFont(new Font("Arial", 1, 25));
+            g.drawString(String.valueOf(provisionalValue), pos[0] + 20 / 4, pos[1] + 27);
+        }
     }
 
-    public void render() {
-        
+    public boolean contains(int x, int y) {
+        return (pos[0] <= x && x < pos[0] + SIZE && pos[1] <= y && y < pos[1] + SIZE);
     }
 
     public void setValue() {
 
     }
 
-    public void setValue(int value) {
+    public void select() {
+        this.selected = true;
+    }
 
+    public void unselect() {
+        this.selected = false;
+    }
+
+    public void setValue(int value) {
+        this.value = value;
+    }
+
+    public int getValue() {
+        return value;
     }
 
     public void setProvisionalValue(int value) {
+        this.provisionalValue = value;
+    }
 
+    public int getProvisionalValue() {
+        return provisionalValue;
+    }
+
+    public int getX() {
+        return this.pos[0];
+    }
+
+    public int getY() {
+        return this.pos[1];
     }
 }
--- src/org/sudoku/GUI.java
+++ src/org/sudoku/GUI.java
@@ -3,8 +3,10 @@ package org.sudoku;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
+import javax.swing.event.MouseInputListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+import java.awt.event.MouseEvent;
 import java.awt.event.KeyEvent;
 
 import java.awt.Dimension;
@@ -12,17 +14,19 @@ import java.awt.Graphics;
 import java.awt.Color;
 
 public class GUI extends JPanel {
-    private static final long serialVersionUID = 1;
-
     /**
      * Holds the main Window and the Sudoku Grid, runs the Game Loop.
      */
+    
+    private static final long serialVersionUID = 1;
+
     private static final String TITLE = "Sudoku Solver";
     private static final int WIDTH = Cell.SIZE * 9 + 4, HEIGHT = Cell.SIZE * 9 + 4;
     private static final int FPS = 60;
     private static final int FRAME_TIME = 1000 / FPS;
 
     private final JFrame window;
+    private final int windowYoffset;
 
     private int[][] data, initialData, solutionData;
     private boolean running, runningSimulation, simulationFinished;
@@ -41,9 +45,11 @@ public class GUI extends JPanel {
         cells = new Cell[9][9];
         for (int row = 0; row <= 8; row++) {
             for (int col = 0; col <= 8; col++) {
-                cells[row][col] = new Cell(new int[]{row, col}, data[row][col]);
+                int[] pos = new int[]{col * Cell.SIZE + 2, row * Cell.SIZE + 2};
+                cells[row][col] = new Cell(pos, data[row][col]);
             }
         }
+        selectedCell = null;
 
         window = new JFrame(TITLE);
         window.setSize(new Dimension(WIDTH, HEIGHT));
@@ -54,24 +60,77 @@ public class GUI extends JPanel {
         window.setVisible(true);
         window.requestFocus();
         // Resize as top bar of the window get counted towards overall size
-        int extraHeight = HEIGHT - window.getContentPane().getSize().height; 
-        window.setSize(new Dimension(WIDTH, HEIGHT + extraHeight));
+        windowYoffset = HEIGHT - window.getContentPane().getSize().height; 
+        window.setSize(new Dimension(WIDTH, HEIGHT + windowYoffset));
 
         window.addKeyListener(new KeyListener() {
             @Override
-            public void keyPressed(KeyEvent e) {
+            public void keyReleased(KeyEvent e) {
                 switch (e.getKeyCode()) {
                     case KeyEvent.VK_ESCAPE:
-                        System.out.println("ESC");
+                        System.exit(1);
                         running = false;
+                        break;
+                    case KeyEvent.VK_ENTER:
+                        System.out.println("ENTER");
+                        if (selectedCell != null && isValueCorrect(selectedCell, selectedCell.getProvisionalValue()))
+                            selectedCell.setValue(selectedCell.getProvisionalValue());
+                        else System.out.println("INVALID");
+                        break;
+                    case KeyEvent.VK_1:
+                        if (selectedCell != null) selectedCell.setProvisionalValue(1);
+                        break;
+                    case KeyEvent.VK_2:
+                        if (selectedCell != null) selectedCell.setProvisionalValue(2);
+                        break;
+                    case KeyEvent.VK_3:
+                        if (selectedCell != null) selectedCell.setProvisionalValue(3);
+                        break;
+                    case KeyEvent.VK_4:
+                        if (selectedCell != null) selectedCell.setProvisionalValue(4);
+                        break;
+                    case KeyEvent.VK_5:
+                        if (selectedCell != null) selectedCell.setProvisionalValue(5);
+                        break;
+                    case KeyEvent.VK_6:
+                        if (selectedCell != null) selectedCell.setProvisionalValue(6);
+                        break;
+                    case KeyEvent.VK_7:
+                        if (selectedCell != null) selectedCell.setProvisionalValue(7);
+                        break;
+                    case KeyEvent.VK_8:
+                        if (selectedCell != null) selectedCell.setProvisionalValue(8);
+                        break;
+                    case KeyEvent.VK_9:
+                        if (selectedCell != null) selectedCell.setProvisionalValue(9);
                         break;
                 }
             }
 
             @Override
-            public void keyReleased(KeyEvent e) {}
+            public void keyPressed(KeyEvent e) {}
             @Override
             public void keyTyped(KeyEvent e){}
+        });
+
+        window.addMouseListener(new MouseInputListener(){
+            @Override
+            public void mouseReleased(MouseEvent e) {
+                selectCellAt(e.getX(), e.getY() - windowYoffset);
+            }
+        
+            @Override
+            public void mouseMoved(MouseEvent e) {}
+            @Override
+            public void mouseDragged(MouseEvent e) {}
+            @Override
+            public void mousePressed(MouseEvent e) {}
+            @Override
+            public void mouseExited(MouseEvent e) {}        
+            @Override
+            public void mouseEntered(MouseEvent e) {}
+            @Override
+            public void mouseClicked(MouseEvent e) {}
         });
     }
 
@@ -105,20 +164,26 @@ public class GUI extends JPanel {
         repaint();
     }
 
-    private void setValue(int row, int col, int value) {
+    private void setValue(Cell cell, int value) {
 
     }
 
-    private boolean isValueCorrect(int row, int col, int value) {
-        return true;
+    private boolean isValueCorrect(Cell cell, int value) {
+        int col = (cell.getX() - 2) / Cell.SIZE;
+        int row = (cell.getY() - 2) / Cell.SIZE;
+        return solutionData[row][col] == value;
     }
 
-    private void selectCellAt(int row, int col) {
-
-    }
-
-    private void confirmProvisional() {
-
+    private void selectCellAt(int x, int y) {
+        if (selectedCell != null) selectedCell.unselect();
+        for (Cell[] row: cells)
+            for(Cell c: row)
+                if (c.contains(x, y)) {
+                    selectedCell = c;
+                    selectedCell.select();
+                    return;
+                }
+        selectedCell = null;
     }
 
     private void startSimulation() {
@@ -126,7 +191,7 @@ public class GUI extends JPanel {
     }
 
     private void cancelSimulation() {
-
+        
     }
 
     @Override
@@ -137,17 +202,21 @@ public class GUI extends JPanel {
 
         g.setColor(Color.BLACK);
         for (int row = 0; row <= 9; row++) {
-            int height = row % 3 == 0 ? 4 : 2;
-            g.fillRect(0, row * Cell.SIZE, WIDTH, height);
-        }
-        for (int col = 0; col <= 9; col++) {
-            int width = col % 3 == 0 ? 4 : 2;
-            g.fillRect(col * Cell.SIZE, 0, width, HEIGHT);
+            if (row % 3 == 0) {
+                g.fillRect(0, row * Cell.SIZE, WIDTH, 4);
+            } else {
+                g.fillRect(0, row * Cell.SIZE + 1, WIDTH, 2);
+            }
         }
 
-        for (Cell[] row: cells)
-            for (Cell c: row) 
-                c.render();
+        for (int col = 0; col <= 9; col++) {
+            if (col % 3 == 0)
+                g.fillRect(col * Cell.SIZE, 0, 4, HEIGHT);
+            else
+                g.fillRect(col * Cell.SIZE + 1, 0, 2, HEIGHT);
+        }
+
+        for (Cell[] row: cells) for(Cell c: row) c.render(g);
     }
 
     private static int[][] deepcopy(int[][] array) {
